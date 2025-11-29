4b65c349d963cee01d00f8ab7040f848eace1228
soujanyaramdeni <soujanyaramdeni.com>
2025-11-29 14:14:31 +0530

    blogimage

--- src/components/BlogCreationPage/blogcreation.jsx
+++ src/components/BlogCreationPage/blogcreation.jsx
@@ -204,6 +204,7 @@ export const CreateBlog = () => {
               }}
             />
             <span className="red_alter">Only images upto 5MB are allowed.</span>
+            <span className="red_alter2">Note: Image size should be 1280 px X 640 px</span>
 
             {preview && (
               <img src={preview} alt="Preview" className="preview-img" />
